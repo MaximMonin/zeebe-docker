5a7cde7380d29415f79aa64650799d5df148bc70
Maxim Monin <maximmonin@gmail.com>
2020-10-08 15:57:53 +0000

    test different approuch but still to slow process instances creation

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-for (( i=0; i<100; ++i)); do
-   docker exec -it zeebe-gateway zbctl --insecure create instance "payment-retrieval" --variables '{"item": "item-xyz", "amount": 555}'
-done
+docker exec -it zeebe-gateway /test.sh
