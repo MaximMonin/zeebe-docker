733ba2b00f76156ed142d5351b294c77bd695ac7
Maxim Monin <maximmonin@gmail.com>
2020-10-08 15:13:21 +0000

    first successful model pass test

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 for (( i=0; i<100; ++i)); do
-curl --location --request POST 'http://localhost:26500/workflows/start' \
---header 'Content-Type: application/json' \
---data-raw '{ "processDefinitionId":"payment-retrieval","variables": {"item": "item-xyz","amount": 555}}'
+   docker exec -it zeebe-gateway zbctl --insecure create instance "payment-retrieval" --variables '{"item": "item-xyz", "amount": 555}'
 done
--- workers/node/payment-retrieval.js
+++ workers/node/payment-retrieval.js
@@ -1,4 +1,4 @@
-const { client, Duration } = require('zeebe-node');
+const { ZBClient, Duration } = require('zeebe-node');
 const axios = require ('axios'); axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 const faker = require ('faker');
 
@@ -17,11 +17,11 @@ const client = new ZBClient(url, {
     connectionTolerance: 3000 // milliseconds
 });
 
-const topology = await client.topology()
-console.log(JSON.stringify(topology, null, 2))
+const topology = client.topology();
+console.log(JSON.stringify(topology, null, 2));
 
 // susbscribe to the task-type: 'charge-card'
-const zbWorker = client.createWorker({
+const zbWorker1 = client.createWorker({
     taskType: 'charge-card',
     taskHandler: Chargehandler,
     failWorkflowOnException: false,
@@ -33,7 +33,7 @@ const zbWorker = client.createWorker({
 })
 
 // susbscribe to the task-type: 'charge-card-premium'
-const zbWorker = client.createWorker({
+const zbWorker2 = client.createWorker({
     taskType: 'charge-card-premium',
     taskHandler: ChargePremiumhandler,
     failWorkflowOnException: false,
@@ -45,7 +45,7 @@ const zbWorker = client.createWorker({
 })
 
 // susbscribe to the task-type: 'generate-item-amount'
-const zbWorker = client.createWorker({
+const zbWorker3 = client.createWorker({
     taskType: 'generate-item-amount',
     taskHandler: Generatehandler,
     failWorkflowOnException: false,
@@ -57,73 +57,19 @@ const zbWorker = client.createWorker({
 })
 
 function Chargehandler(task, complete, worker) {
-    worker.log('Task variables', job.variables)
-
-    // Task worker business logic goes here
-    const updateToBrokerVariables = {
-	updatedProperty: 'newValue',
-    }
-    complete.success(updateToBrokerVariables)
-
   // Get a process variable
-  const amount = task.variables.get('amount');
-  const item = task.variables.get('item');
+  const amount = task.variables.amount;
+  const item = task.variables.item;
 
   console.log(`Charging credit card with an amount of ${amount}€ for the item '${item}'...` );
-  console.log(`Process: ` + url + '/process-instance/' + task.processInstanceId )
 
-  axios.get(url + '/process-instance/' + task.processInstanceId ).then(response => {
-     const data = response.data;
-     console.log('Process data: ' + JSON.stringify(data));
+  complete.success();
+};
 
-     // Complete the task
-     taskService.complete(task);
-  })
-  .catch(function (error) {
-    if (error.response) {
-      // Request made and server responded
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('Error', error.message);
-    }
-
-    // Handle a Failure
-    if (Math.random() > 0.1)
-    {
-    // repeat again and again
-      taskService.handleFailure(task, {
-        errorMessage: "Cannot exctract process id data",
-        errorDetails: error.response.data.message,
-        retries: 1,
-        retryTimeout: 1000
-      });
-    }
-    else {
-    // Create incident
-      taskService.handleFailure(task, {
-        errorMessage: "Cannot exctract process id data",
-        errorDetails: error.response.data.message,
-        retries: 0,
-        retryTimeout: 1000
-      });
-    }
-  });
-
-});
-
-// susbscribe to the topic: 'charge-card-premium'
-client.subscribe('charge-card-premium', {processDefinitionKey: bpm}, async function({ task, taskService }) {
-  // Put your business logic here
-
+function ChargePremiumhandler(task, complete, worker) {
   // Get a process variable
-  const amount = task.variables.get('amount');
-  const item = task.variables.get('item');
+  const amount = task.variables.amount;
+  const item = task.variables.item;
 
   console.log(`Premium charging credit card with an amount of ${amount}€ for the item '${item}'...`);
 
@@ -134,30 +80,26 @@ client.subscribe('charge-card-premium', {processDefinitionKey: bpm}, async funct
      console.log('Telegram answer: ' + JSON.stringify(data));
 
      // Complete the task
-     taskService.complete(task);
+     complete.success();
   })
   .catch(function (error) {
     if (error.response) {
-      console.log(error.response.data);
+      console.log('Telegram error: ' + error.response.data);
     } 
     else {
       console.log('error sending data to telegram');
     }
-    // Complete the task
-    taskService.complete(task);
+    // repeat task
+    complete.failure('Something goes wrong', 1);
   });
-});
+};
 
 
-// susbscribe to the topic: 'generate-item-amount'
-client.subscribe('generate-item-amount', {processDefinitionKey: bpm},  async function({ task, taskService }) {
-
+function Generatehandler(task, complete, worker) {
   console.log(`Generating amount and item for process...`);
 
-  const processVariables = new Variables();
-  processVariables.set("amount", Number(faker.fake('{{finance.amount}}')));
-  processVariables.set('item', faker.fake('{{commerce.product}}'));
-
-  // Complete the task
-  await taskService.complete(task, processVariables);
-});
+  complete.success({ 
+      amount: Number(faker.fake('{{finance.amount}}')),
+      item: faker.fake('{{commerce.product}}')
+  });
+};
