3467aad39d9dc4eab6f4f92b0bda9bca8b6fd911
Maxim Monin <maximmonin@gmail.com>
2020-10-10 10:03:38 +0000

    redisign of nodejs client - added rest api to start workflow

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker exec -it zeebe-gateway /test.sh
+for (( i=0; i<100; ++i)); do
+  curl -H "Content-Type: application/json" -X POST -d '{"variables": {"amount": 555, "item": "item-xyz" } }' http://localhost:2595/process-definition/key/payment-retrieval/start
+done
--- workers/node/payment-retrieval.js
+++ workers/node/payment-retrieval.js
@@ -1,12 +1,23 @@
 const { ZBClient, Duration } = require('zeebe-node');
+const express = require('express')
 const axios = require ('axios'); axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 const faker = require ('faker');
 
 const url = process.env.ZeebeURL || 'gateway:26500';
 const bpm = 'payment-retrieval';
 const timeout = process.env.ResponseTimeout || 10000;
+const loglevel = process.env.LogLevel || 'INFO';
 const bot = process.env.TelegramBot;
 const channel = process.env.TelegramChannel;
+
+console.log('Zeebe Node worker is starting...')
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const server = app.listen(port, () =>
+  console.log(`Zeebe REST handler listening on port ${port}!`)
+);
 
 const client = new ZBClient(url, {
     retry: true,
@@ -17,62 +28,120 @@ const client = new ZBClient(url, {
     connectionTolerance: 3000 // milliseconds
 });
 
-const topology = client.topology();
-console.log(JSON.stringify(topology, null, 2));
+// For docker enviroment it catch docker compose down/restart commands
+// The signals we want to handle
+// NOTE: although it is tempting, the SIGKILL signal (9) cannot be intercepted and handled
+var signals = {
+  'SIGHUP': 1,
+  'SIGINT': 2,
+  'SIGTERM': 15
+};
+// Do any necessary shutdown logic for our application here
+const shutdown = (signal, value) => {
+  server.close(() => {
+    console.log(`Zeebe Node worker stopped`);
+    process.exit(128 + value);
+  });
+};
+// Create a listener for each of the signals that we want to handle
+Object.keys(signals).forEach((signal) => {
+  process.on(signal, () => {
+    console.log(`Zeebe Node worker is shutdowning`);
+    shutdown(signal, signals[signal]);
+  });
+});
 
-// susbscribe to the task-type: 'charge-card'
-const zbWorker1 = client.createWorker({
+
+async function main() {
+  // subscribe to the task-type: 'charge-card'
+  const zbWorker1 = client.createWorker({
     taskType: 'charge-card',
     taskHandler: Chargehandler,
     failWorkflowOnException: false,
     maxJobsToActivate: 10,
     timeout: timeout,
-    loglevel: 'INFO',
+    loglevel: loglevel,
     onReady: () => console.log(`Worker connected to 'charge-card'`),
     onConnectionError: () => console.log(`Worker disconnected from 'change-card'`)
-})
+  });
 
-// susbscribe to the task-type: 'charge-card-premium'
-const zbWorker2 = client.createWorker({
+  // subscribe to the task-type: 'charge-card-premium'
+  const zbWorker2 = client.createWorker({
     taskType: 'charge-card-premium',
     taskHandler: ChargePremiumhandler,
     failWorkflowOnException: false,
     maxJobsToActivate: 10,
     timeout: timeout,
-    loglevel: 'INFO',
+    loglevel: loglevel,
     onReady: () => console.log(`Worker connected to 'charge-card-premium'`),
     onConnectionError: () => console.log(`Worker disconnected from 'change-card-premium'`)
-})
+  });
 
-// susbscribe to the task-type: 'generate-item-amount'
-const zbWorker3 = client.createWorker({
+  // subscribe to the task-type: 'generate-item-amount'
+  const zbWorker3 = client.createWorker({
     taskType: 'generate-item-amount',
     taskHandler: Generatehandler,
     failWorkflowOnException: false,
     maxJobsToActivate: 10,
     timeout: timeout,
-    loglevel: 'INFO',
+    loglevel: loglevel,
     onReady: () => console.log(`Worker connected to 'generate-item-amount'`),
     onConnectionError: () => console.log(`Worker disconnected from 'generate-item-amount'`)
-})
+  });
+
+  app.post('/process-definition/:key/start', startWorkflow);
+  app.post('/process-definition/key/:key/start', startWorkflow);
+};
+
+async function startWorkflow (req, res) {
+  const workflowKey = req.params.key;
+  const iParams = Object.assign({}, req.query, req.body);
+
+  console.log ('Starting workflow...' + workflowKey + ' ' + JSON.stringify(iParams));
+  const vars = iParams.variables;
+
+  const { workflowInstanceKey } = await client.createWorkflowInstance(
+      workflowKey,
+      vars
+  );
+  var obj = {workflowInstanceKey: workflowInstanceKey};
+  res.status(200).end(JSON.stringify(obj));
+}
+
+main ();
 
 function Chargehandler(task, complete, worker) {
+  const { workflowInstanceKey, bpmnProcessId, elementId } = task;
+
+  if (bpmnProcessId != bpm) {
+    complete.forwarded();
+    return;
+  }
+
   // Get a process variable
   const amount = task.variables.amount;
   const item = task.variables.item;
 
-  console.log(`Charging credit card with an amount of ${amount}€ for the item '${item}'...` );
-
+  console.log(`Charging credit card with an amount of ${amount}€ for the item '${item}' ` + ' Process :' + workflowInstanceKey.toString() );
   complete.success();
 };
 
 function ChargePremiumhandler(task, complete, worker) {
+  const { workflowInstanceKey, bpmnProcessId, elementId } = task;
+
+  if (bpmnProcessId != bpm) {
+    complete.forwarded();
+    return;
+  }
+
   // Get a process variable
   const amount = task.variables.amount;
   const item = task.variables.item;
 
-  console.log(`Premium charging credit card with an amount of ${amount}€ for the item '${item}'...`);
+  console.log(`Premium charging credit card with an amount of ${amount}€ for the item '${item}' ` + ' Process :' + workflowInstanceKey.toString() );
+  complete.success();
 
+/*
   // send message to telegram channel
   axios.post( 'https://api.telegram.org/bot' + bot + '/sendMessage', {chat_id: channel,  parse_mode: 'HTML', 
                text: 'Bingo! We got ' + amount + '€ for ' + item  }).then(response => {
@@ -92,11 +161,19 @@ function ChargePremiumhandler(task, complete, worker) {
     // repeat task
     complete.failure('Something goes wrong', 1);
   });
+*/
 };
 
 
 function Generatehandler(task, complete, worker) {
-  console.log(`Generating amount and item for process...`);
+  const { workflowInstanceKey, bpmnProcessId, elementId } = task;
+
+  if (bpmnProcessId != bpm) {
+    complete.forwarded();
+    return;
+  }
+
+  console.log(`Generating amount and item for Process ` + workflowInstanceKey.toString());
 
   complete.success({ 
       amount: Number(faker.fake('{{finance.amount}}')),
