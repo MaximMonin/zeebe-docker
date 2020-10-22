d262990ce7898b40ae4caa5e1f3f02c9ecffaae2
Maxim Monin <maximmonin@gmail.com>
2020-10-22 15:36:26 +0000

    zeebe uses 0/25 and node0 file optimization

--- deploy_test_bpmn.sh
+++ deploy_test_bpmn.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 curl -H "Content-Type: application/json" -X POST -d '{"upload": "/zeebe/bpms/examples/payment-retrieval.bpmn"}' http://localhost:2595/deployment/create
+curl -H "Content-Type: application/json" -X POST -d '{"upload": "/zeebe/bpms/examples/search-domain.bpmn"}' http://localhost:2595/deployment/create
